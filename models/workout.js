384b7e8e6442029716496967d588cc01e9434ee7
Sanusi Cole <sanusi1612@icloud.com>
2021-09-09 16:49:30 -0400

    adds workout model

--- models/workout.js
+++ models/workout.js
@@ -2,22 +2,28 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const transactionSchema = new Schema({
+const workoutSchema = new Schema({
+  day: { type: Date, default: Date.now },
   name: {
     type: String,
     trim: true,
-    required: "Enter a name for transaction"
-  },
-  value: {
-    type: Number,
-    required: "Enter an amount"
-  },
-  date: {
-    type: Date,
-    default: Date.now
+    required: "Enter a name for workout"
   }
-});
+  });
 
-const Transaction = mongoose.model("Transaction", transactionSchema);
+const Workout = mongoose.model("workout", workoutSchema);
 
-module.exports = Transaction;
+module.exports = {Workout};
+
+
+// day: new Date().setDate(new Date().getDate()-8),
+// exercises: [
+//   {
+//     type: "resistance",
+//     name: "Push Press",
+//     duration: 25,
+//     weight: 185,
+//     reps: 8,
+//     sets: 4
+//   }
+// ]
