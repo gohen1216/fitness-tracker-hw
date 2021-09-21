bad501b5e3530647b93280bbb00629f54238ebd2
Sanusi Cole <sanusi1612@icloud.com>
2021-09-21 14:02:03 -0400

    add put routesd

--- server.js
+++ server.js
@@ -57,9 +57,37 @@ app.post('/api/workouts', (req, res) => {
     res.status(400).json(err);
   });
     
-  
+      
  
 });
+
+
+app.get('/api/workouts/range', (req, res) => {
+  
+  Workout.find({})
+  .sort({ date: -1 })
+  .then(dbWorkouts => {
+    res.json(dbWorkouts);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+    
+
+});
+app.put('/api/workouts/:workoutid', (req, res) => {
+  const workoutid=req.params.workoutid
+  Workout.findByIdAndUpdate(workoutid,  { $push: { exercises: req.body } },)
+  .then(dbWorkouts => {
+    res.json(dbWorkouts);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+    
+
+});
+
 
 // Starts the server to begin listening
 
